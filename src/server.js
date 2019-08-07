ca22b7a059ebf575b3175eb0f789de68129dc0d0
Gabriel Henrique <gabrielhenrique.dev@outlook.com>
2019-08-06 23:47:37 -0300

    feat: list dev, new dev, like, dislike, match

---
+++ src/server.js
@@ -0,0 +1,18 @@
+require('dotenv/config');
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+const server = express();
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`, {
+    useNewUrlParser: true
+});
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);
